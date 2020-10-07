46c555832e034d4e8d3f265f7f18ca17d4959afc
dlzjc <993094702@qq.com>
2020-10-07 11:06:13 +0800

    second time

--- jump.cpp
+++ jump.cpp
@@ -1,8 +1,17 @@
-#include <stdio.h>
-#include <windows.h>
-int main()
-{
-    printf("Hello World\n");
+#include<iostream>
+#include<vector>
+#include<windows.h>
+using namespace std;
+int jump_ans(int N){
+	if(N==1||N==0) return 1;
+	else return jump_ans(N-1)+jump_ans(N-2);
+}
+int main(void){
+	int N,ans;
+	cout << "input the number of steps:" <<endl;
+	cin >> N;
+	ans=jump_ans(N);
+	cout << "the methods to reach order "<< N<< " are as follow:"<< ans  <<endl; 
     system("pause");
-    return 0;
+	return 0;
 }
